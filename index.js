6ca2957d3374dfd1ad62f1fd0105fd9175851b0c
ZahraO1 <zahra.masroor6@gmail.com>
2022-03-30 14:29:20 -0400

    Created a function to keep the code simple

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
+function newImage(src,x,y){
+    let element = document.createElement('img')
+    element.src = src;
+    element.style.position = 'fixed';
+    element.style.left = x +'px';
+    element.style.bottom = y + 'px';
+    document.body.append(element)
+}
+
+//let greenCharacter = document.createElement('img')
+/* greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+document.body.append(greenCharacter) */
+newImage('assets/green-character.gif',100,100)
+
+//let pineTree = document.createElement('img')
+/* pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree) */
+newImage('assets/pine-tree.png',450,200)
+
